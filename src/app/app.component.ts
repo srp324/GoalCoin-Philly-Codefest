f34b45e825122af0f42177e5e8c9a08cd2671965
sagarpatel <srp324@drexel.edu>
2018-04-15 03:47:42 -0400

    Imported Data Service and created getABI Method to frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
+// Import the DataService
+import { DataService } from './data.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  // Define a users property to hold our user data
+  //users: Array<any>;
+  users: any;
+  dataService: DataService;
+
+  // Create an instance of the DataService through dependency injection
+  constructor(private _dataService: DataService) 
+  {
+    // Access the Data Service's getUsers() method we defined
+    this.dataService = _dataService;
+  }
+
+  getABI() {
+    this._dataService.getABI().subscribe(res => this.users = res);
+    console.log("Got ABI!");
+    console.log(this.users);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
 
+// Import the Http Module and our Data Service
+import { HttpModule } from '@angular/http';
+import { DataService } from './data.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,                // <-Add HttpModule
+    RouterModule
   ],
-  providers: [],
+  providers: [DataService],   // <-Add DataService
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+
+  
+}
